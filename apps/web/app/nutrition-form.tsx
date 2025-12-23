4436ca026b6719ce95c5c4ec09fc7da7c0724890
hungknow <hungbuiknow@gmail.com>
2025-12-23 09:36:13 +0700

    web use Nutrition react

---
+++ apps/web/app/nutrition-form.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { WeightEvaluation } from "@community-nutrition/who-child-growth-standards-react"
+
+export const NutritionForm = () => {
+    return (
+        <div>
+            <WeightEvaluation />
+        </div>
+    )
+}
