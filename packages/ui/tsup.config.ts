25c04613474d99bf8904048dd87da22fe22fa5d9
hungknow <hungbuiknow@gmail.com>
2025-12-23 07:48:40 +0700

    ui package build component individually

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from "tsup"
+import { fixImportsPlugin } from "esbuild-fix-imports-plugin"
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/styles/globals.css"],
+  entry: [
+    "src/index.ts",
+    "src/styles/globals.css",
+    "src/components/ui/**/*.tsx",
+    "src/lib/**/*.ts",
+  ],
   format: ["cjs", "esm"],
   dts: true,
+  bundle: false,
   splitting: false,
   sourcemap: true,
   clean: true,
   external: ["react", "react-dom", "react/jsx-runtime"],
   treeshake: true,
   minify: false,
+  esbuildPlugins: [fixImportsPlugin()],
   esbuildOptions(options) {
     options.tsconfig = "./tsconfig.json"
   },
