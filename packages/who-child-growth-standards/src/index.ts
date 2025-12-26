76ad0e03287075e53df112aedc0a3662c641eff7
hungknow <hungbuiknow@gmail.com>
2025-12-26 08:58:20 +0700

    Add data for length-for-age and height-for-age

--- packages/who-child-growth-standards/src/index.ts
+++ packages/who-child-growth-standards/src/index.ts
@@ -1,6 +1,9 @@
 export * from "./types";
-export * from "./types";
 export * from "./weight-for-length-d3js";
 export * from "./weight-for-length-0-to-2-years";
 export * from "./weight-for-length-2-to-5-years";
 export * from "./weight-for-length";
+
+export * from "./length-for-age-0-to-13-weeks";
+export * from "./height-for-age-0-to-2-years";
+export * from "./height-for-age-2-to-5-years";
--- packages/who-child-growth-standards/src/types.ts
+++ packages/who-child-growth-standards/src/types.ts
@@ -15,9 +15,9 @@ export interface WeightForLength {
     sd1: number;
     sd2: number;
     sd3: number;
-  }
-  
-  export enum WeightForLengthEvalulationStatus {
+}
+
+export enum WeightForLengthEvalulationStatus {
     BelowSd3Neg = "below-sd3-neg",
     BetweenSd3NegAndSd2Neg = "between-sd3-neg-and-sd2-neg",
     BetweenSd2NegAndSd1Neg = "between-sd2-neg-and-sd1-neg",
@@ -26,6 +26,32 @@ export interface WeightForLength {
     BetweenSd1AndSd2 = "between-sd1-and-sd2",
     BetweenSd2AndSd3 = "between-sd2-and-sd3",
     AboveSd3 = "above-sd3",
-  }
+}
 
-  
+// Used for child under 13 weeks
+export interface LengthForAge {
+    week: number;
+    l: number;
+    m: number;
+    s: number;
+    sd3neg: number;
+    sd2neg: number;
+    sd1neg: number;
+    sd0: number;
+    sd1: number;
+    sd2: number;
+}
+
+// For the child over 13 weeks
+export interface HeighthForAge {
+    month: number;
+    l: number;
+    m: number;
+    s: number;
+    sd3neg: number;
+    sd2neg: number;
+    sd1neg: number;
+    sd0: number;
+    sd1: number;
+    sd2: number;
+}
